8ec795bb7a78acbdeb55ec9e97fc7cfdd16c5d3f
Mahendra Gurav <mahendra.gurav@autolabs.ai>
2019-05-07 14:13:05 +0530

    test

--- src/components/analytics/Coins.js
+++ src/components/analytics/Coins.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Highcharts from "highcharts";
 import addHeatmapModule from "highcharts/modules/heatmap";
 import addTreemapModule from "highcharts/modules/treemap";
+import axios from "axios";
 import {
   HighchartsChart,
   withHighcharts,
@@ -61,100 +62,142 @@ class Coins extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      treemapData: [
-        {
-          name: "AXD",
-          id: "AXD",
-          value: 3,
-          color: "#33FFFF"
-        },
+      treeData: null
+      // treemapData: [
+      //   {
+      //     name: "AXD",
+      //     id: "AXD",
+      //     value: 3,
+      //     color: "#33FFFF"
+      //   },
 
-        {
-          name: "BTC",
-          id: "BTC",
-          value: 1,
-          color: "#FF334C"
-        },
+      //   {
+      //     name: "BTC",
+      //     id: "BTC",
+      //     value: 1,
+      //     color: "#FF334C"
+      //   },
 
-        {
-          name: "XYT",
-          id: "XYT",
-          value: 2,
-          color: "#3633FF"
-        },
-        {
-          name: "ETH",
-          id: "ETH",
-          value: 1,
-          color: "#FF334C"
-        },
-        {
-          name: "OTHERS",
-          value: 1,
-          id: "OTHERS",
-          color: "#A9A4A9"
-        },
-        {
-          name: "OTHERS-1",
-          value: 1,
-          id: "OTHERS1",
-          parent: "OTHERS",
-          color: "#FF334C"
-        },
-        {
-          name: "OTHERS-2",
-          value: 1,
-          id: "OTHERS2",
-          parent: "OTHERS",
-          color: "#CFD801"
-        },
-        {
-          name: "OTHERS-3",
-          value: 1,
-          id: "OTHERS2",
-          parent: "OTHERS",
-          color: "#C6AA0D"
-        },
-        {
-          name: "OTHERS-4",
-          value: 3,
-          id: "OTHERS2",
-          parent: "OTHERS",
-          color: "#0DC667"
-        },
-        {
-          name: "OTHERS-5",
-          value: 10,
-          id: "OTHERS2",
-          parent: "OTHERS",
-          color: "#B2C60D"
-        },
-        {
-          name: "OTHERS-6",
-          value: 2,
-          id: "OTHERS2",
-          parent: "OTHERS",
-          color: "#0DC6C6"
-        }
-      ]
+      //   {
+      //     name: "XYT",
+      //     id: "XYT",
+      //     value: 2,
+      //     color: "#3633FF"
+      //   },
+      //   {
+      //     name: "ETH",
+      //     id: "ETH",
+      //     value: 1,
+      //     color: "#FF334C"
+      //   },
+      //   {
+      //     name: "OTHERS",
+      //     value: 1,
+      //     id: "OTHERS",
+      //     color: "#A9A4A9"
+      //   },
+      //   {
+      //     name: "OTHERS-1",
+      //     value: 1,
+      //     id: "OTHERS1",
+      //     parent: "OTHERS",
+      //     color: "#FF334C"
+      //   },
+      //   {
+      //     name: "OTHERS-2",
+      //     value: 1,
+      //     id: "OTHERS2",
+      //     parent: "OTHERS",
+      //     color: "#CFD801"
+      //   },
+      //   {
+      //     name: "OTHERS-3",
+      //     value: 1,
+      //     id: "OTHERS2",
+      //     parent: "OTHERS",
+      //     color: "#C6AA0D"
+      //   },
+      //   {
+      //     name: "OTHERS-4",
+      //     value: 3,
+      //     id: "OTHERS2",
+      //     parent: "OTHERS",
+      //     color: "#0DC667"
+      //   },
+      //   {
+      //     name: "OTHERS-5",
+      //     value: 10,
+      //     id: "OTHERS2",
+      //     parent: "OTHERS",
+      //     color: "#B2C60D"
+      //   },
+      //   {
+      //     name: "OTHERS-6",
+      //     value: 2,
+      //     id: "OTHERS2",
+      //     parent: "OTHERS",
+      //     color: "#0DC6C6"
+      //   }
+      // ]
     };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    var url = new URL("https://cmc-node-app.herokuapp.com/charts/getchartdata");
+    // var url = new URL(
+    //   "https://gist.githubusercontent.com/whawker/809cae1781f25db5f3c2dd7cee93b017/raw/94ca755307ac5651686467b5fa1844659b5817a3/data.json"
+    // );
+
+    axios
+      .get(url, {
+        data: {
+          params: {
+            type: "TreeMap",
+            category: "TopCoinsBlockchain"
+          }
+        },
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*"
+        }
+      })
+      .then(res => {
+        console.log("sucesssss");
+        console.log(res.data);
+      });
+
+    //Working
+    // fetch(
+    //   "https://gist.githubusercontent.com/whawker/809cae1781f25db5f3c2dd7cee93b017/raw/94ca755307ac5651686467b5fa1844659b5817a3/data.json"
+    // )
+    //   .then(res => {
+    //     if (res.ok) {
+    //       console.log("sucess!!");
+    //       return res.json();
+    //     }
+    //     console.log("fail!!");
+    //     //throw new Error("Network response was not ok.");
+    //   })
+    //   .then(json => {
+    //     this.setState({
+    //       treeData: formatData(json)
+    //     });
+    //   });
+  }
 
   onClick = e => {
-    alert(
-      "Id :" +
-        e.point.node.id +
-        "  Value : " +
-        e.point.node.val +
-        " Lavel " +
-        e.point.node.level
-    );
+    // alert(
+    //   "Id :" +
+    //     e.point.node.id +
+    //     "  Value : " +
+    //     e.point.node.val +
+    //     " Lavel " +
+    //     e.point.node.level
+    // );
   };
 
   render() {
-    const treeData = this.state.treemapData;
+    const treeData = this.state.treeData;
     if (!treeData) return null;
 
     const drilldown = [
